55f02b3d9d9bf88df77a6d1949e5cbfdd0cac355
Sangmin Jeon <jeonsm0404@gmail.com>
2024-03-16 23:01:29 +0900

    무한반복 기능 생성

--- start_page.py
+++ start_page.py
@@ -1,12 +1,20 @@
-while True:
-    print("게임을 시작하시겠습니까?(Y/N)", end = " ")
-    isGameStart = input()
+import multiprocessing as mp
 
-    if isGameStart == 'Y' or 'y':
-        print("게임을 시작합니다.")
-        break
-    elif isGameStart == 'N' or 'n':
-        print("게임을 종료합니다.")
-        break
-    else:
-        print("'Y'나 'N'을 입력해주세요.")
+def Game():
+    print("가위바위보")
+
+
+if __name__ == '__main__':
+    while True:
+        print("게임을 시작하시겠습니까?(Y/N)", end = " ")
+        isGameStart = input()
+
+        if isGameStart == 'Y' or isGameStart == 'y':
+            print("게임을 시작합니다.")
+            pid = mp.Process(target=Game)
+            pid.start(); pid.join()
+        elif isGameStart == 'N' or isGameStart == 'n':
+            print("게임을 종료합니다.")
+            break
+        else:
+            print("'Y'나 'N'을 입력해주세요.")
